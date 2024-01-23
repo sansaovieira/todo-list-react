ea7c10e0565313e793d6f5802c42c505e2ed311b
Sansão Vieira <sansao.vieira@global.ntt>
2024-01-23 02:25:26 -0300

    (feat): Creating the task list

---
+++ src/components/TodoForms.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+const TodoForms = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!value || !category) {
+      alert("Preecha todos os campos!"); 
+      return;
+    }
+    
+    addTodo(value, category)
+    setValue("");
+    setCategory("");
+  } 
+
+  return (
+    <div className='todo-form'>
+      <h2>Criar tarefa: </h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text"
+          placeholder='Digite o título'
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
+          <option value="">Selecione uma categoria</option>
+          <option value="Trabalho">Trabalho</option>
+          <option value="Pessoal">Pessoal</option>
+          <option value="Estudos">Estudos</option>
+        </select>
+        <button type='submit'>Criar tarefa</button>
+      </form>
+    </div>
+  )
+}
+
+export default TodoForms
